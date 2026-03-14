c74d5237355f1c53f0f3f277a4afb796bc33f30e
Jeremy7002 <jerejames1.12@gmail.com>
2026-03-14 12:30:16 +0530

    STAGE 1 : COMPLETE

--- src/organizer/classifier.py
+++ src/organizer/classifier.py
@@ -1,6 +1,11 @@
 import os
 import json
 
+# Load categories from config
+with open("config/config.json", "r") as file:
+    config = json.load(file)
+    categories = config["categories"]
+
 def classify_file(file_path):
     """
     Determines the category of a file based on its extension.
@@ -10,10 +15,6 @@ def classify_file(file_path):
     _, extension = os.path.splitext(file_path)
 
     extension = extension.lower()
-    # Load categories from config
-    with open("config/config.json", "r") as file:
-        config = json.load(file)
-        categories = config["categories"]
 
     # Classification rules
     for category, ext_list in categories.items():
